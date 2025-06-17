d3212b807435e8fa170756661175daaf0efeb1f3
oㅅo <151032546+WinterI5Coming@users.noreply.github.com>
2025-06-17 18:03:13 +0900

    fix: change CourseController name to TestCourseContoller (#13)

--- src/main/java/com/goott5/lms/test/mapper/course/TestCourseMapper.java
+++ src/main/java/com/goott5/lms/test/mapper/course/TestCourseMapper.java
@@ -4,11 +4,10 @@ package com.goott5.lms.test.mapper.course;
 import com.goott5.lms.test.domain.course.CourseInfoDTO;
 import com.goott5.lms.user.domain.UserVO;
 import java.util.List;
-import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface CourseMapper {
+public interface TestCourseMapper {
 
   // 관리자 강좌 리스트 가져오기
   List<String> selectCourseListForAdmin(Boolean isInProgress);
